dfccd1310cbfc8123eb8fd828c335d1b2ee73b8c
CamronLinux <holy_hades@ymail.com>
2024-09-03 23:18:40 -0600

    creating custom third personcontroller

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@export var cam_sens : float
+@onready var camera_origion: Node3D = $"Camera Origion"
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -10,16 +12,17 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+	var look_direction = Input.get_vector("R_Left", "R_Right", "R_Up", "R_Down")
+	camera_origion.rotation += Vector3(look_direction.y, look_direction.x, 0) * cam_sens
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("L_Left", "L_Right", "L_Up", "L_Down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
