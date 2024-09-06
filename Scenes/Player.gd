016e7028a0182767dce96f0bc40f87f67b539a55
CamronLinux <holy_hades@ymail.com>
2024-09-06 16:29:24 -0600

    readjusted forward vector of player to match camera

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody3D
 
-
+var vec3 := Vector3()
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @export var cam_sens : float
-@onready var camera_origion: Node3D = $"Camera Origion"
+@export var camera_origion: Node3D
+@export var camera_3d: Camera3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -12,8 +13,9 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	var look_direction = Input.get_vector("R_Left", "R_Right", "R_Up", "R_Down")
-	camera_origion.rotation += Vector3(look_direction.y, look_direction.x, 0) * cam_sens
+	var look_direction = Input.get_vector("R_Left", "R_Right", "R_Up", "R_Down").normalized()
+	camera_origion.rotation_degrees += Vector3(look_direction.y, look_direction.x, 0) * cam_sens
+	print(camera_origion.rotation_degrees.x)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -23,7 +25,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("L_Left", "L_Right", "L_Up", "L_Down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (camera_origion.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
